4fc86f7743fd82904e4028fcfeafb7f9103a835f
Yanislav Yanev <85335574+YIYanev20@users.noreply.github.com>
2022-02-23 16:52:41 +0200

    Refinements

--- Monument-Hunter/scripts/WorldMap.js
+++ Monument-Hunter/scripts/WorldMap.js
@@ -1,36 +1,30 @@
 $(document).ready( _ => {
 
-	var level = 0; //declare here, change by going to a level, use as the level's style and it's battle's specifications
+	var level = 0; //declare here
 
 	$('#bg').on('click', _ => {
-		alert('It works!');
-		$(this).css('font-color', 'red'); // Fix this line later!
+		$(this).css('font-color', 'red'); // Fix this line alter!
 		level = 1;
-		console.log(level); // a check to see if var level works
 	});
 	
 	$('#fr').on('click', _ => {
 		$(this).css('font-color', 'red'); // Fix this line later!
 		level = 2;
-		console.log(level); // a check to see if var level works
 	});
 
 	$('#uk').on('click', _ => {
 		$(this).css('font-color', 'red'); // Fix this line later!
 		level = 3;
-		console.log(level); // a check to see if var level works
 	});
 
 	$('#it').on('click', _ => {
 		$(this).css('font-color', 'red'); // Fix this line later!
 		level = 4;
-		console.log(level); // a check to see if var level works
 	});
 
 	$('#ru').on('click', _ => {
 		$(this).css('font-color', 'red'); // Fix this line later!
 		level = 5;
-		console.log(level); // a check to see if var level works
 	});
 	
 });
